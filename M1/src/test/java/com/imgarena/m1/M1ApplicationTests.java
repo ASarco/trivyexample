811c2b6991c344b3846cc3d0c8f81c8c071b4d4a
Alex Sarco <alex.sarco@imgarena.com>
2024-02-23 16:35:14 +0000

    Initial commit

---
+++ M1/src/test/java/com/imgarena/m1/M1ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.imgarena.m1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M1ApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
---
+++ M2/src/main/java/com/imgarena/m2/M2Application.java
@@ -0,0 +1,12 @@
+package com.imgarena.m2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(M2Application.class, args);
+  }
+}
